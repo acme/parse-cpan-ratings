7b75cc670e36b27c3a6053d0652eab894b53aab9
Leon Brocard <acme@astray.com>
2009-07-02 11:18:17 +0100

    add docs

--- lib/Parse/CPAN/Ratings.pm
+++ lib/Parse/CPAN/Ratings.pm
@@ -50,3 +50,50 @@ sub ratings {
     my $self = shift;
     return values %{ $self->db };
 }
+
+__END__
+
+=head1 NAME
+
+Parse::CPAN::Ratings - Parse CPAN ratings
+
+=head1 SYNOPSIS
+
+  my $ratings
+      = Parse::CPAN::Ratings->new( filename => 't/all_ratings_100.csv' );
+
+  my $rating = $ratings->rating('Archive-Zip');
+  print $rating->distribution . "\n"; # Archive-Zip
+  print $rating->rating . "\n";       # 3.8
+  print $rating->review_count . "\n"; # 6
+
+  my @all_ratings = $ratings->ratings;
+
+=head1 DESCRIPTION
+
+CPAN ratings is a web site where programmers can rate CPAN modules:
+
+  http://cpanratings.perl.org/
+  
+It provides a file containing the average ratings at:
+
+http://cpanratings.perl.org/csv/all_ratings.csv
+
+This module provides a simple interface to that file.
+
+=head1 SEE ALSO
+
+L<Parse::CPAN::Ratings::Rating>.
+
+=head1 AUTHOR
+
+Leon Brocard <acme@astray.com>.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009, Leon Brocard
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it or modify it
+under the same terms as Perl itself.
